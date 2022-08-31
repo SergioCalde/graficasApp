64a28a240ffac5025f0c9c9218f62d5e14da8cc3
SergioCalde <sergio.calderon.dm@outlook.com>
2022-08-30 18:19:25 -0600

    Add doughnut chart and change colors and offsets.

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
   styleUrls: ['./dona.component.css']
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ],
+        backgroundColor: ['#248CF2','#26CBFC','#2DE6E1'],
+        hoverBackgroundColor: ['#4EA1F2','#51D5FC','#55E6E2'],
+        borderColor: ['#248CF2','#26CBFC','#2DE6E1'],
+        hoverBorderColor: ['#248CF2','#26CBFC','#2DE6E1'],
+        hoverOffset: 15        
+      },
+      
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 }
